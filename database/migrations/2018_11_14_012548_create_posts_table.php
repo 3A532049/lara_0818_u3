6db2629f720f412626c03f7a8c89d955f1e14554
3a532049 <shelly213334@gmail.com>
2019-01-19 10:06:43 +0800

    調整posts資料表結構的兩個指令應放up()當中，down()要放相反的指令。

--- database/migrations/2018_11_14_012548_create_posts_table.php
+++ database/migrations/2018_11_14_012548_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration
+class AddIsFeatureInPostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->text('content');
-            $table->timestamps();
+        Schema::table('posts', function (Blueprint $table) {
+
+            $table->string('title',500)->change();
+            $table->boolean('is_feature')->default(false)->after('content');
         });
     }
 
@@ -28,6 +27,9 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::table('posts', function (Blueprint $table) {
+            $table ->string('title') ->change();
+            $table ->dropColumn('is_feature') ;
+        }
     }
 }
